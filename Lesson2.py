135ad6a1d6ccbafd4eeddeaee64d8ebeab8beb84
MelanieSmid <melanie.m.smid@gmail.com>
2019-10-19 16:54:45 -0400

    Math and strings

---
+++ Lesson2.py
@@ -0,0 +1,31 @@
+value = input("Please enter your name:") #getting a user's name
+print(value)
+
+age = input("Enter your age:") #getting user age
+future_age = eval(age) + 10
+print(future_age)
+
+age_again = eval(input ("Enter your age:")) #eval different
+extra_age = age_again + 25
+print (extra_age)
+
+import math #Math Library
+print("The value of Pi is")
+print(math.pi)
+
+print("There's even another way to")
+print("do power, with math.pow")
+print(math.pow(2,2))
+
+print("New " + "York") #adding strings
+
+print("and over " * 3) 
+
+word = "My words are important!" #len is length of string
+print(len(word))
+
+phrase = "Python Rocks"  #indexing and slicing
+print(phrase [0])
+print(phrase [1:3])
+print(phrase [7:])
+
